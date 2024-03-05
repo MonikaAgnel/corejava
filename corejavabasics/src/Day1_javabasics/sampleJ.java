9f455924493fd946e75b37f6fd5cd304f2fe7c44
MONIKA <MONIKA@LAPTOP-V5JP6AR2>
2024-03-05 15:45:57 +0530

    it is a core ja       va practical programs

---
+++ corejavabasics/src/Day1_javabasics/sampleJ.java
@@ -0,0 +1,15 @@
+package Day1_javabasics;
+import java.util.*;
+public class sampleJ {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("enter the values of a and b:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("addition of a="+a+" and b="+b+" is:" +c);
+	}     
+
+}
