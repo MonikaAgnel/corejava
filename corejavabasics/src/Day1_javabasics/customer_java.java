ed26e0a5eca60711dce08a8a86c482a40ef45ed6
MONIKA <MONIKA@LAPTOP-V5JP6AR2>
2024-03-07 14:08:26 +0530

    This is a day3 java code

---
+++ corejavabasics/src/Day1_javabasics/customer_java.java
@@ -0,0 +1,28 @@
+package Day1_javabasics;
+
+public class customer_java {
+ 
+		
+            private int cid;
+            private String name;
+            private String address;
+			public int getCid() {
+				return cid;
+			}
+			public void setCid(int cid) {
+				this.cid = cid;
+			}
+			public String getName() {
+				return name;
+			}
+			public void setName(String name) {
+				this.name = name;
+			}
+			public String getAddress() {
+				return address;
+			}
+			public void setAddress(String address) {
+				this.address = address;
+			}
+}
+           
---
+++ corejavabasics/src/Day1_javabasics/executor.java
@@ -0,0 +1,12 @@
+package Day1_javabasics;
+
+public class executor {
+ public static void main(String[] args) {
+	 customer_java c1 =new customer_java();
+	 c1.setCid(112);
+	 c1.setName("Moni");
+	 c1.setAddress("pondy");
+	System.out.println("Customer id:"+c1.getCid()+"customer name:"+c1.getName()+"customer Address:"+c1.getAddress());
+	    
+ }
+}  
