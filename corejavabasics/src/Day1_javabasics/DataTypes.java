889b84fd2044c8f37c2611029ea916a6c162f48e
MONIKA <MONIKA@LAPTOP-V5JP6AR2>
2024-03-10 22:51:31 +0530

    This is a code for Datatype in java

---
+++ corejavabasics/src/Day1_javabasics/DataTypes.java
@@ -0,0 +1,23 @@
+package Day1_javabasics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		int a1=10/3;
+		float a2=101/61;
+		double a3=10d/6d;
+		float a4=5/2.5f;
+		
+		System.out.println("a1="+a1);
+		System.out.println("a2="+a2);
+		System.out.println("a3="+a3);
+		System.out.println("a4="+a4);
+		
+		int mark=512;
+		double percentage=mark*0.46f;
+		System.out.println("percentage="+percentage);
+	
+		}
+
+}
