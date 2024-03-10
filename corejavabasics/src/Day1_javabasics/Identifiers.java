650343d5c8aab014989b4954bbb2e44557bfa439
MONIKA <MONIKA@LAPTOP-V5JP6AR2>
2024-03-10 23:05:12 +0530

    This is a identifier code in Java

---
+++ corejavabasics/src/Day1_javabasics/Identifiers.java
@@ -0,0 +1,12 @@
+package Day1_javabasics;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		int $num=20;
+		System.out.println("value of variable is:"+$num);
+		String studentName="Moni";
+		System.out.println("value is:"+studentName);
+		}
+
+}
