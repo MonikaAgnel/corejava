a7fa67d75f1c57c9fe45c98504db0b82b5d26148
MONIKA <MONIKA@LAPTOP-V5JP6AR2>
2024-03-10 21:16:46 +0530

    This is a code for character

---
+++ corejavabasics/src/Day1_javabasics/Char.java
@@ -0,0 +1,29 @@
+package Day1_javabasics;
+
+public class Char {
+
+	public static void main(String[] args) {
+		
+		// assigning single character literal
+		char ch = 'a';
+		System.out.println(ch);
+
+		// assigning number to char
+		char ch1 = 65;
+		System.out.println(ch1);
+
+		// assigning unicode to char
+		char ch2= '\u00A7';
+		System.out.println(ch2);
+
+		// Unicode representation
+		char ch3 = '\u20AC';
+		System.out.println(ch3);
+
+		// ASCI code representation
+		int a = 'A';
+		System.out.println(a);
+
+	}
+
+}
