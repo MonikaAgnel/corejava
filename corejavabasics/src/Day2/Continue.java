10bcef99763f73df57cddfd55043d885d6afdbc8
MONIKA <MONIKA@LAPTOP-V5JP6AR2>
2024-03-12 01:41:48 +0530

    This is the code for break,continue and decision making

---
+++ corejavabasics/src/Day2/Continue.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=5;i<15;i++) {
+	if(i%2 !=0)
+		continue;
+	System.out.println(i);
+		
+}
+	}
+
+}
